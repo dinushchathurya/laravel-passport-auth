bc84b630d773b3b62e69fda7a81097ee63450e1a
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-23 13:07:47 +0530

    :sparkles: Create function for getting single post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,5 +15,21 @@ class PostController extends Controller
             'data' => $posts
         ]);
     }
+    
+    public function show($id)
+    {
+        $post = auth()->user()->posts()->find($id);
  
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found '
+            ], 400);
+        }
+ 
+        return response()->json([
+            'success' => true,
+            'data' => $post->toArray()
+        ], 400);
+    }
 }
