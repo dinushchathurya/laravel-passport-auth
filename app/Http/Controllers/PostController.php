d0ad34bfcf46ee28a9fb1ec76f6af28a4c538274
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-23 13:08:47 +0530

    :sparkles: Create function to create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,4 +32,27 @@ class PostController extends Controller
             'data' => $post->toArray()
         ], 400);
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+ 
+        $post = new Post();
+        $post->title = $request->title;
+        $post->description = $request->description;
+ 
+        if (auth()->user()->posts()->save($post))
+            return response()->json([
+                'success' => true,
+                'data' => $post->toArray()
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not added'
+            ], 500);
+    }
 }
