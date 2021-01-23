e3f64a2e47ce499e5f2f611ef1f5445bb108501c
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-23 13:13:25 +0530

    :sparkles: Define api endpoints

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('posts', PostController::class);
 });
